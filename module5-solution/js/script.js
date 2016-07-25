7177fdfd1afd0be9a8757f03cb85b9f9f5b34cd7
Nandhini Lakuduva <Nandhini@Nandhinis-MBP.home>
2016-07-24 19:21:43 -0500

    Module 5 Assignment

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -102,10 +102,12 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = "\'" + chooseRandomCategory(categories).short_name + "\'";
-      console.log("my " + chosenCategoryShortName);
-      
-      //chosenCategoryShortName = chooseRandomCategory(categories);
+           
+      var chosenCategoryShortName = chooseRandomCategory(categories);
+
+       var chosenCategoryShortName2 = "\'" + chooseRandomCategory(categories).short_name + "\'";
+      console.log("my " + chosenCategoryShortName2);
+
        
       chosenCategoryShortName = JSON.stringify(chosenCategoryShortName.short_name);
 
